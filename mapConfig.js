c689bc129c6c22f904f198ff25a753b03e985896
Nima Boscarino <nboscarino@hatfieldgroup.com>
2017-04-28 14:29:23 -0700

    change stamen basemap to something with less contrast

--- mapConfig.js
+++ mapConfig.js
@@ -8,7 +8,7 @@ var map = L.map('map', {
 map.doubleClickZoom.disable();
 map.options.minZoom = 3;
 // NOTE: do not use stamen without API key for production
-var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
 	minZoom: 0,
